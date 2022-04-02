b6e88731627a249ca7adfc8f80c05e2086a6658e
Krystian Kaźmierczak <krystian.kazmierczak91@gmail.com>
2022-04-02 15:00:34 +0200

    gdzie jest krzyz

--- src/PracaWGrupie.java
+++ src/PracaWGrupie.java
@@ -1,14 +1,4 @@
 public class PracaWGrupie {
-    // Jeden komentarz
-    // ja się nie boję myśleć o brzydkich słowach
-    //todo:  opowiedzieć pastę o mentosie
-//Pasta nie jest najprostsza,ale takie jest życie
-
-    // mentos fuu
 
     // † ameno
-
-
-    // Zobacz, rakieta! 3=======>    :D
-    // no to próbujemy przejść do codzienności
 }
