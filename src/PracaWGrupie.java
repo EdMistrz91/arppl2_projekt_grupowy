f24a68d42901f95d0fa548f4f7eebdb37a6b23d8
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-02 14:53:40 +0200

    zmiany

--- src/PracaWGrupie.java
+++ src/PracaWGrupie.java
@@ -2,6 +2,6 @@ public class PracaWGrupie {
     // Jeden komentarz
     // ja się nie boję myśleć o brzydkich słowach
     //todo:  opowiedzieć pastę o mentosie
-
+//Pasta nie jest najprostsza,ale takie jest życie
     // Zobacz, rakieta! 3=======>    :D
 }
